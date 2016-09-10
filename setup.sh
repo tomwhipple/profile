07a68da5a71c880ec5b0466052e7c47d11bfd2ca
Tom Whipple <mail@tomwhipple.com>
2016-09-10 18:12:55 -0500

    add vim plugins

--- setup.sh
+++ setup.sh
@@ -3,10 +3,21 @@
 REL_DIR=`dirname $0`
 cd $REL_DIR
 
-PROFILE_DIR=`pwd`
+PROFILE_DIR=`pwd -P`
+
 cd $HOME
 
 echo 'exec /bin/bash -l' > .profile
 ln -s "$PROFILE_DIR/bash_profile" .bash_profile
 ln -s "$PROFILE_DIR/tmux.conf" .tmux.conf
+ln -s "$PROFILE_DIR/vimrc" .vimrc
+
+VIM_DIR="$HOME/.vim/bundle" 
+mkdir -p "$VIM_DIR"
+
+VUNDLE_DIR="$VIM_DIR/Vundle.vim"
+if ! [ -d $VUNDLE_DIR ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+	vim +PluginInstall +qall
+fi
 
