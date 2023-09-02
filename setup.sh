d08b02b019acb5407eb4d35601f5a15127e77607
Tom Whipple <mail@tomwhipple.com>
2023-09-02 12:07:25 -0600

    bugfix: .profile definition was wrong

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ PROFILE_DIR=`pwd -P`
 
 cd $HOME
 
-echo 'exec /bin/bash -l' > .profile
 ln -s "$PROFILE_DIR/bash_profile" .bash_profile
 ln -s "$PROFILE_DIR/tmux.conf" .tmux.conf
 ln -s "$PROFILE_DIR/vimrc" .vimrc
